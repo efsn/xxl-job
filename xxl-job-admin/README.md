027f2209ac52229fa00c418e127566c3bfb10b3e
arthur.chen <efsn.chan@gmail.com>
2021-03-25 22:35:34 +0800

    feat: remove unused dependency

---
+++ xxl-job-admin/README.md
@@ -0,0 +1,9 @@
+
+
+```
+docker build -t elmi/xxl-job:2.3.0 .
+
+docker push elmi/xxl-job:2.3.0
+
+
+```
--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/DynamicJobApiController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/DynamicJobApiController.java
@@ -4,8 +4,6 @@ import com.xxl.job.admin.controller.annotation.PermissionLimit;
 import com.xxl.job.admin.core.conf.XxlJobAdminConfig;
 import com.xxl.job.admin.core.model.XxlJobInfo;
 import com.xxl.job.admin.service.XxlJobService;
-import com.xxl.job.core.biz.AdminBiz;
-import com.xxl.job.core.biz.model.HandleCallbackParam;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.util.GsonTool;
 import com.xxl.job.core.util.XxlJobRemotingUtil;
@@ -17,21 +15,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
- * Created by xuxueli on 17/5/10.
+ * @author elmi
+ * @date 2021/03/25
  */
 @Controller
 @RequestMapping("/api/job")
 public class DynamicJobApiController {
 
     @Resource
-    private AdminBiz adminBiz;
-
-    @Resource
     private XxlJobService xxlJobService;
-
 
     /**
      * api
@@ -47,15 +41,15 @@ public class DynamicJobApiController {
 
         // valid
         if (!"POST".equalsIgnoreCase(request.getMethod())) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, HttpMethod not support.");
+            return new ReturnT<>(ReturnT.FAIL_CODE, "invalid request, HttpMethod not support.");
         }
         if (uri == null || uri.trim().length() == 0) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping empty.");
+            return new ReturnT<>(ReturnT.FAIL_CODE, "invalid request, uri-mapping empty.");
         }
         if (XxlJobAdminConfig.getAdminConfig().getAccessToken() != null
                 && XxlJobAdminConfig.getAdminConfig().getAccessToken().trim().length() > 0
                 && !XxlJobAdminConfig.getAdminConfig().getAccessToken().equals(request.getHeader(XxlJobRemotingUtil.XXL_JOB_ACCESS_TOKEN))) {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "The access token is wrong.");
+            return new ReturnT<>(ReturnT.FAIL_CODE, "The access token is wrong.");
         }
 
         // services mapping
@@ -66,7 +60,7 @@ public class DynamicJobApiController {
         } else if ("remove".equals(uri)) {
             return xxlJobService.remove(GsonTool.fromJson(data, XxlJobInfo.class).getId());
         } else {
-            return new ReturnT<String>(ReturnT.FAIL_CODE, "invalid request, uri-mapping(" + uri + ") not found.");
+            return new ReturnT<>(ReturnT.FAIL_CODE, "invalid request, uri-mapping(" + uri + ") not found.");
         }
 
     }
